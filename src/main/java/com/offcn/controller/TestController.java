3ffbe77b1af21bc7175dd826c9378400fbe3324e
wuxiaofan <wuxiaofan@wxf.com>
2020-12-06 20:05:32 +0800

    新增一个死循环

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -3,12 +3,17 @@ package com.offcn.controller;
 /**
  * @Author: wxf
  * @Date: 2020/12/6 17:53
- * @Description:
- */
+        * @Description:
+        */
 public class TestController {
     public static void main(String[] args) {
         System.out.println("hello this world");
         //update by wxf
         System.out.println("update");
+
+        //add by wxf
+        while (true){
+
+        }
     }
 }
